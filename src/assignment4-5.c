8abd95ac03d1c9eb4ac25f9482c246dfa6cb20f5
Ajinkya Dahale <ajinkyadahale@gmail.com>
2018-04-23 14:30:34 -0400

    Added functions to make images
    Still need to get hang of all the binary format questions

--- src/assignment4-5.c
+++ src/assignment4-5.c
@@ -24,7 +24,9 @@
 
 #define ALIVE 1
 #define DEAD  0
+#define CELLS_PER_PIXEL 4
 typedef unsigned short int celltype;
+typedef unsigned short int pixel_t;
 
 /***************************************************************************/
 /* Global Vars *************************************************************/
@@ -41,9 +43,13 @@ typedef unsigned short int celltype;
 
 void my_swap(celltype** a, celltype** b);
 
+void draw_img(celltype** univ, pixel_t** img, size_t nrows, size_t ncols);
+
 void fprint_self(FILE* out_file, celltype** univ, size_t nrows, size_t ncols);
 
 void fprint_univ(MPI_File out_file, celltype** univ, size_t nrows, size_t ncols);
+
+void fprint_pixels(MPI_File out_file, pixel_t** img, size_t nrows, size_t ncols);
 
 /** Play one tick of a game of life serially with given array of known number
  *  of rows and columns. Assumed that rows wrap around, but first and last
@@ -62,12 +68,17 @@ int main(int argc, char *argv[])
 {
 //    int i = 0;
   size_t nrows = 8, ncols = 16, i, j;
+  size_t nirows = nrows / CELLS_PER_PIXEL;
+  size_t nicols = ncols / CELLS_PER_PIXEL;
   celltype **univ;
+  pixel_t **img;
   FILE *out_file = NULL;
   MPI_File mpi_out_fh;
+  MPI_File mpi_img_fh;
   char out_filename[16];
     int mpi_myrank;
     int mpi_commsize;
+
 // Example MPI startup and using CLCG4 RNG
     MPI_Init( &argc, &argv);
     MPI_Comm_size( MPI_COMM_WORLD, &mpi_commsize);
@@ -76,6 +87,10 @@ int main(int argc, char *argv[])
     MPI_File_open(MPI_COMM_WORLD, "out_all",
 		  MPI_MODE_CREATE | MPI_MODE_WRONLY,
 		  MPI_INFO_NULL, &mpi_out_fh);
+
+    MPI_File_open(MPI_COMM_WORLD, "out_all_img",
+		  MPI_MODE_CREATE | MPI_MODE_WRONLY,
+		  MPI_INFO_NULL, &mpi_img_fh);
 
 // Init 16,384 RNG streams - each rank has an independent stream
     InitDefault();
@@ -99,11 +114,20 @@ int main(int argc, char *argv[])
       univ  = (celltype **)malloc(sizeof(celltype *) *(nrows+2));
       univ[0] = (celltype *)malloc(sizeof(celltype) * ncols *(nrows+2));
 
+      /* Defining initial state */
+      /* TODO: Randomize */
       for(i = 0; i < (nrows+2); i++) {
 	univ[i] = (*univ + ncols * i);
 	for (j = 0; j < ncols; ++j) {
 	  univ[i][j] = DEAD;
 	}
+      }
+
+      img = (pixel_t **) malloc(sizeof(pixel_t *) * (nirows+2));
+      img[0] = (pixel_t *) malloc(sizeof(pixel_t) * nicols * nirows);
+
+      for(i = 0; i < nirows; i++) {
+	img[i] = (*img + nicols * i);
       }
 
       if (0 == mpi_myrank) {
@@ -118,16 +142,13 @@ int main(int argc, char *argv[])
       for (tick = 0; tick < 32; ++tick) {
 	fprintf(out_file, "Rank %d, tick %ld:\n", mpi_myrank, tick);
 	fprint_self(out_file, univ, nrows, ncols);
-        /* /\* TODO: perform ghosting *\/ */
-        /* for (j = 0; j < ncols; ++j) { */
-        /*   univ[0][j] = univ[nrows][j]; */
-        /*   univ[nrows+1][j] = univ[1][j]; */
-        /* } */
 	/* TODO: play one tick */
 	play_gol_mpi(univ, nrows, ncols, tick);
       }
 
       fprint_univ(mpi_out_fh, univ, nrows, ncols);
+      draw_img(univ, img, nrows, ncols);
+      fprint_pixels(mpi_img_fh, img, nirows, nicols);
       fprintf(out_file, "Rank %d, tick %ld:\n", mpi_myrank, tick);
       fprint_self(out_file, univ, nrows, ncols);
     }
@@ -144,6 +165,32 @@ int main(int argc, char *argv[])
 /***************************************************************************/
 /* Other Functions - You write as part of the assignment********************/
 /***************************************************************************/
+
+void my_swap(celltype** a, celltype** b) {
+  celltype* temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+void draw_img(celltype** univ, pixel_t** img, size_t nrows, size_t ncols)
+{
+  /* TODO: erase old image? */
+  size_t nirows = nrows/CELLS_PER_PIXEL;
+  size_t nicols = ncols/CELLS_PER_PIXEL;
+  size_t ci, cj, ii, ij;
+  for (ii = 0; ii < nirows; ++ii) {
+    for (ij = 0; ij < nicols; ++ij) {
+      img[ii][ij] = 0;
+      for (ci = 0; ci < CELLS_PER_PIXEL; ++ci) {
+	for (cj = 0; cj < CELLS_PER_PIXEL; ++cj) {
+	  /* Here we ghost, one more time */
+	  img[ii][ij] += univ[ii*CELLS_PER_PIXEL + ci + 1][ij*CELLS_PER_PIXEL + cj];
+	}
+      }
+    }
+  }
+
+}
 
 void fprint_self(FILE* out_file, celltype** univ, size_t nrows, size_t ncols)
 {
@@ -170,10 +217,15 @@ void fprint_univ(MPI_File out_file, celltype** univ, size_t nrows, size_t ncols)
     MPI_File_write_at(out_file, mpi_myrank * nrows * ncols * sizeof(celltype), univ[1], nrows * ncols, MPI_UNSIGNED_SHORT, &status);
 }
 
-void my_swap(celltype** a, celltype** b) {
-  celltype* temp = *a;
-  *a = *b;
-  *b = temp;
+void fprint_pixels(MPI_File out_file, pixel_t** img, size_t nrows, size_t ncols)
+{
+    int mpi_myrank;
+    int mpi_commsize;
+    MPI_Status status;
+    MPI_Comm_size( MPI_COMM_WORLD, &mpi_commsize);
+    MPI_Comm_rank( MPI_COMM_WORLD, &mpi_myrank);
+    /* Note: univ[1] because of the ghost row */
+    MPI_File_write_at(out_file, mpi_myrank * nrows * ncols * sizeof(pixel_t), img[0], nrows * ncols, MPI_UNSIGNED_SHORT, &status);
 }
 
 void play_gol(celltype** univ, size_t nrows, size_t ncols)
